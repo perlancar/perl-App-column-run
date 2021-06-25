51c5ee5b8af7bc867367ba03a4493ce1bb89976d
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2021-06-25 09:44:29 +0700

    [incompatible change] Rename option --arg(s) to --common-arg(s); Add options --common-env(s), --args-arrays, --envs-arrays

--- lib/App/column/run.pm
+++ lib/App/column/run.pm
@@ -42,8 +42,8 @@ except with the following differences:
 
 * passing adjusted COLUMNS environment to commands so they can adjust output;
 
-* passing common arguments to all commands.
-
+* passing common arguments and environment variables to all commands (as well as
+  allowing each command to have its unique arguments or environment variables).
 
 _
     args => {
@@ -56,11 +56,37 @@ _
             pos => 0,
             slurpy => 1,
         },
-        args => {
-            summary => 'Common arguments to pass to each program',
+        common_args => {
+            summary => 'Common arguments to pass to each command',
+            description => <<'_',
+
+If `--args-arrays` is also set, then the common arguments will be added first,
+then the per-command arguments.
+
+_
             'x.name.is_plural' => 1,
-            'x.name.singular' => 'arg',
+            'x.name.singular' => 'common_arg',
             schema => ['array*', of=>'str*'],
+        },
+        args_arrays => {
+            summary => 'Arguments to give to each command (an array of arrays of strings)',
+            schema => ['array*', of=>'aos*'],
+            description => <<'_',
+
+If `--common-args` is also set, then the common arguments will be added first,
+then the per-command arguments.
+
+_
+        },
+        common_envs => {
+            summary => 'Common environment variables to pass to each command',
+            'x.name.is_plural' => 1,
+            'x.name.singular' => 'common_env',
+            schema => ['hash*', of=>'str*'],
+        },
+        envs_arrays => {
+            summary => 'Environment variables to give to each command (an array of hashes of strings)',
+            schema => ['array*', of=>'hos*'],
         },
     },
     'cmdline.skip_format' => 1,
@@ -69,14 +95,27 @@ _
         {url=>'prog:column', summary=>'Unix utility to fill columns with list'},
         {url=>'prog:diff', summary=>'The --side-by-side (-y) option display files in two columns'},
     ],
+    examples => [
+        {
+            summary => 'Compare JSON vs Perl Data::Dump vs YAML dump, side by side',
+            src => 'cat ~/samples/bookstore.json | COLOR=1 column-run pp-json json2dd json2yaml',
+            src_plang => 'bash',
+            test => 0,
+            'x.doc.show_result' => 0,
+        },
+    ],
 };
 sub column_run {
     require IPC::Run;
     require ShellQuote::Any::PERLANCAR;
 
     my %args = @_;
+    #use DD; dd \%args;
     my $commands = delete $args{commands};
-    my $command_args = delete $args{args};
+    my $common_command_args = delete($args{common_args}) // [];
+    my $command_args_arrays = delete($args{args_arrays}) // [];
+    my $common_envs = delete($args{common_envs}) // {};
+    my $command_envs_arrays = delete($args{envs_arrays}) // [];
 
     Text::Column::Util::show_texts_in_columns(
         %args,
@@ -87,20 +126,32 @@ sub column_run {
             # simple way: one by one and grab the whole output. in the future we
             # might do this parallel and line-by-line.
 
+            my %orig_env = %ENV;
+
             my $stdin = "";
             unless (-t STDIN) {
                 local $/;
                 $stdin = <STDIN>;
             }
 
-            local $ENV{COLUMNS} = $gargs{column_width};
-
             my @texts; # ([line1-from-cmd1, ...], [line1-from-cmd2, ...], ...)
             for my $i (0..$#{$commands}) {
                 my $cmd = $commands->[$i];
-                if ($command_args) {
-                    $cmd .= " " . ShellQuote::Any::PERLANCAR::shell_quote(@{ $command_args });
+                if (@$common_command_args) {
+                    $cmd .= " " . ShellQuote::Any::PERLANCAR::shell_quote(@{ $common_command_args });
                 }
+                if ($command_args_arrays->[$i] && @{ $command_args_arrays->[$i] }) {
+                    $cmd .= " " . ShellQuote::Any::PERLANCAR::shell_quote(@{ $command_args_arrays->[$i] });
+                }
+                log_trace "Command[%d]: %s", $i, $cmd;
+
+                %ENV = %orig_env;
+                $ENV{COLUMNS} = $gargs{column_width};
+                $ENV{$_} = $common_envs->{$_} for keys %$common_envs;
+                log_trace("Setting env for command[%d]: %s=%s", $i, $_, $common_envs->{$_}, $i) for keys %$common_envs;
+                $ENV{$_} = $command_envs_arrays->[$i]{$_} for keys %{ $command_envs_arrays->[$i] // {} };
+                log_trace("Setting env for command[%d]: %s=%s", $i, $_, $command_envs_arrays->[$i]{$_}) for keys %{ $command_envs_arrays->[$i] // {} };
+
                 my ($out, $err);
                 IPC::Run::run(
                     sub {
@@ -113,6 +164,9 @@ sub column_run {
                 );
                 $texts[$i] = $out;
             }
+
+            %ENV = %orig_env;
+
             \@texts;
         }, # _gen_texts
     );
@@ -121,7 +175,10 @@ sub column_run {
 1;
 #ABSTRACT:
 
-=head1 append:SEE ALSO
+=head1 DESCRIPTION
+
+
+=head1 SEE ALSO
 
 Terminal multiplexers: B<tmux>, B<screen>.
 
